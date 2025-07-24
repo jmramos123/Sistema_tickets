b7504bd32039f420e91ead81373971795661bdcc
jmramos123 <159inspirado@gmail.com>
2025-07-24 12:50:21 -0400

    nginx try 16

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -15,9 +15,19 @@ class TrustProxies extends Middleware
     protected $proxies = '*';
 
     /**
-     * Trust the standard X‑Forwarded‑* headers.
+     * The headers that should be used to detect proxies.
+     *
+     * We trust:
+     *  - X-Forwarded-For
+     *  - X-Forwarded-Host
+     *  - X-Forwarded-Port
+     *  - X-Forwarded-Proto
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = 
+          Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO;
 }
