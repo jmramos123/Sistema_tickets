923e5e265774e09fdc1ab291faf5e7862fca481d
jmramos123 <159inspirado@gmail.com>
2025-07-25 02:03:30 -0400

    nginx try 38

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # Force Laravel to forget any cached config and pick up the Render env vars
 php artisan config:clear
 php artisan cache:clear
+php artisan route:clear
 
 # Run migrations and seeders against the DB_CONNECTION provided by Render (pgsql)
 php artisan migrate:refresh --force --seed
